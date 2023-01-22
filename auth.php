cdcf1f4ab3faa80dffa3dcd67387586aab28f103
Kalloor, Eric Abraham <eric.kalloor@cerner.com>
2023-01-22 14:45:18 +0530

    quth

--- auth.php
+++ auth.php
@@ -19,6 +19,6 @@ curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 $resp = curl_exec($curl);
 curl_close($curl);
 var_dump($resp);
-echo($resp);
-echo(var_dump($resp));
+// echo($resp);
+// echo(var_dump($resp));
 ?>
