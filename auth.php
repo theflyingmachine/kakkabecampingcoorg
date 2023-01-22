30ae045b1cf98f052adeccf3dace523e7cc8b5b9
Kalloor, Eric Abraham <eric.kalloor@cerner.com>
2023-01-22 14:19:53 +0530

    quth

--- auth.php
+++ auth.php
@@ -1,22 +1,32 @@
-
 <?php
-$url = 'https://api.instagram.com/oauth/access_token';
+$url = "https://api.instagram.com/oauth/access_token";
 echo('Please wait while we fetch your access_token');
-$data = array('client_id' => '746720933176046', 'client_secret' => '6fbcc075c5afd5d8556a8bdcc814d5f3', 'grant_type' => 'authorization_code', 'redirect_uri' => 'https://cybergirl.in/auth.php', 'code' => $_GET['code']);
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+echo('\n');
 echo($_GET['code']);
-echo('Checking.....');
-// use key 'http' even if you send the request to https://...
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data)
-    )
+$headers = array(
+   "Accept: application/json",
+   "Content-Type: application/json",
 );
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE) { /* Handle error */ }
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-var_dump($result);
+$data = <<<DATA
+{
+  "client_id": 746720933176046,
+  "client_secret": "6fbcc075c5afd5d8556a8bdcc814d5f3",
+  "grant_type": "authorization_code",
+  "redirect_uri": "https://cybergirl.in/auth.php",
+  "code": $_GET['code'],
+}
+DATA;
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+echo('Checking.....');
+$resp = curl_exec($curl);
+curl_close($curl);
+
+echo $resp;
 ?>
-
