1f2c8e0b2efc1f059fcb817cd80f385e44df0893
Kalloor, Eric Abraham <eric.kalloor@cerner.com>
2023-01-22 14:41:29 +0530

    quth

--- auth.php
+++ auth.php
@@ -1,32 +1,24 @@
 <?php
 $url = "https://api.instagram.com/oauth/access_token";
 echo('Please wait while we fetch your access_token');
-$curl = curl_init();
+$curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-echo('\n');
-echo($_GET['code']);
+
 $headers = array(
-   "Accept: application/json",
-   "Content-Type: application/json",
+  "Content-Type: application/x-www-form-urlencoded",
 );
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-$data = <<<DATA
-{
-  "client_id": 746720933176046,
-  "client_secret": "6fbcc075c5afd5d8556a8bdcc814d5f3",
-  "grant_type": "authorization_code",
-  "redirect_uri": "https://cybergirl.in/auth.php",
-  "code": $_GET['code'],
-}
-DATA;
-
-curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+$data = "client_id=746720933176046&client_secret=6fbcc075c5afd5d8556a8bdcc814d5f3&grant_type=authorization_code&redirect_uri=https://cybergirl.in/auth.php&code=".$_GET['code'];
+echo($data);
 echo('Checking.....');
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
 $resp = curl_exec($curl);
 curl_close($curl);
-
-echo $resp;
+var_dump($resp);
+echo($resp);
+echo(var_dump($resp));
 ?>
