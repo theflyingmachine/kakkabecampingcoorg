9e092194730eb902c596e530b3d8051da5c90000
Kalloor, Eric Abraham <eric.kalloor@cerner.com>
2023-01-24 09:56:59 +0530

    Button

--- auth.php
+++ auth.php
@@ -1,6 +1,5 @@
 <?php
 $url = "https://api.instagram.com/oauth/access_token";
-echo('Please wait while we fetch your access_token');
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_POST, true);
@@ -14,10 +13,6 @@ curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
 $data = "client_id=746720933176046&client_secret=6fbcc075c5afd5d8556a8bdcc814d5f3&grant_type=authorization_code&redirect_uri=https://cybergirl.in/auth.php&code=".$_GET['code'];
 // echo($data);
-echo("\r\n");
-echo('Checking.....');
-echo($_GET['code']);
-echo("\r\n");
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 
 // $resp = curl_exec($curl);
